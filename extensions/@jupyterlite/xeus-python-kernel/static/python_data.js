6f6cb99712c0272ffceb3d88b66bd26936bfa68e
ccha23 <42616371+ccha23@users.noreply.github.com>
2022-08-11 14:40:50 +0800

    Update documentation

--- extensions/@jupyterlite/xeus-python-kernel/static/python_data.js
+++ extensions/@jupyterlite/xeus-python-kernel/static/python_data.js
@@ -19,7 +19,7 @@
         // web worker
         PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
       }
-      var PACKAGE_NAME = '/var/folders/_8/czkg9d7930j0gbgqnmmhj_6r0000gn/T/tmp1z11lktd/python_data.data';
+      var PACKAGE_NAME = '/var/folders/_8/czkg9d7930j0gbgqnmmhj_6r0000gn/T/tmp06_k049z/python_data.data';
       var REMOTE_PACKAGE_BASE = 'python_data.data';
       if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
         Module['locateFile'] = Module['locateFilePackage'];
@@ -822,9 +822,9 @@ Module['FS_createPath']("/tmp/xeus-python-kernel/envs/xeus-python-kernel/lib/pyt
             compressedData['data'] = byteArray;
             assert(typeof Module['LZ4'] === 'object', 'LZ4 not present - was your app build with -sLZ4?');
             Module['LZ4'].loadPackage({ 'metadata': metadata, 'compressedData': compressedData }, true);
-            Module['removeRunDependency']('datafile_/var/folders/_8/czkg9d7930j0gbgqnmmhj_6r0000gn/T/tmp1z11lktd/python_data.data');
+            Module['removeRunDependency']('datafile_/var/folders/_8/czkg9d7930j0gbgqnmmhj_6r0000gn/T/tmp06_k049z/python_data.data');
       };
-      Module['addRunDependency']('datafile_/var/folders/_8/czkg9d7930j0gbgqnmmhj_6r0000gn/T/tmp1z11lktd/python_data.data');
+      Module['addRunDependency']('datafile_/var/folders/_8/czkg9d7930j0gbgqnmmhj_6r0000gn/T/tmp06_k049z/python_data.data');
 
       if (!Module.preloadResults) Module.preloadResults = {};
 
